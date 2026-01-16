2cb8aa716eec60e28e0df17d2a354227b4410779
Nishant Choudhary <nishantchoudhary@Nishants-MacBook-Air.local>
2026-01-16 12:42:11 +0530

    env file added

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: VITE_API_URL,
   withCredentials: true,
 });
 
